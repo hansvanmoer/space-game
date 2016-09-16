4aabc9d09f9accb232ed25690e46d5a47abb2d7f
Hans Van Moer <hansvm.projects@gmail.com>
2016-09-16 20:24:46 +0200

    Removed duplicate source tree

--- io/IOError.cpp
+++
@@ -1,6 +0,0 @@
-#include "IOError.h"
-
-using namespace Core;
-using namespace std;
-
-IOError::IOError(const string &message) : runtime_error(message){}
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int arg_cunt, const char **args){
-	cout << "Hello universe!" << endl;
-}
